0fa98622302ebc238cea832138a24b4e9bafb6d5
sendmenas <giedrius.bielskis@gmail.com>
2017-03-22 09:46:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// request permission on page load
+document.addEventListener('DOMContentLoaded', function () {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+function notifyMe() {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+		var message = document.getElementById("notification");
+		var notification = new Notification('Dukascopy', {
+		icon: 'http://www.lesolstice.ch/wp-content/uploads/2016/06/Swiss-flag.png',
+		body: message.value,
+	});  
+
+    notification.onclick = function () {
+      window.open("https://www.dukascopy.com/");      
+    };
+  }
+}
