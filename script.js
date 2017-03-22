30c717652162cbf33f644a2cb5d4d13d188b3cf8
sendmenas <giedrius.bielskis@gmail.com>
2017-03-22 10:15:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ function notifyMe() {
 		var notification = new Notification('Dukascopy', {
 		icon: 'http://www.lesolstice.ch/wp-content/uploads/2016/06/Swiss-flag.png',
 		body: message.value,
-	});  
+	});
+	setTimeout(notification.close.bind(notification), 3000);
 
     notification.onclick = function () {
       window.open("https://www.dukascopy.com/");      
